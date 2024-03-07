671ee2027389db4a011e79a798ab32283668247d
joelmirsiqueira <136918049+joelmirsiqueira@users.noreply.github.com>
2024-03-07 20:15:21 -0300

    Add files via upload

--- Lista5/src/questao3/Circulo.java
+++ Lista5/src/questao3/Circulo.java
@@ -1,5 +1,15 @@
 package questao3;
 
 public class Circulo {
-
+    private double raio;
+    
+    public Circulo (double raio) {
+        this.raio = raio;
+    }
+    public double area () {
+        return Math.PI * raio * raio;
+    }
+    public double perimetro () {
+        return 2 * Math.PI * raio;
+    }
 }
---
+++ Lista5/src/questao3/Main.java
@@ -0,0 +1,47 @@
+package questao3;
+
+import java.util.Scanner;
+
+public class Main {
+    private static Circulo c1 = new Circulo(0);
+    
+    public static void main(String[] args) {
+        int opcao;
+        do{
+            opcao = memu();
+            Sys
+        } while(opcao != 0);
+    }
+    public static int memu() {
+        Scanner sc = new Scanner(System.in);
+        int opcao;
+        
+        System.out.println("Escolha a opção:");
+        System.out.println("1. Criar circulo");
+        System.out.println("2. Caucular área");
+        System.out.println("3. Calcular perímetro");
+        System.out.println("0. Sair");
+        
+        opcao = sc.nextInt();
+        switch(opcao){
+            case 1:
+                System.out.print("Raio do círculo: ");
+                double raio = sc.nextDouble();
+                c1 = new Circulo(raio);
+                break;
+            case 2:
+                System.out.println("Área = " + c1.area());
+                break;
+            case 3:
+                System.out.println("Perímetro = " + c1.perimetro());
+                break;
+            case 0:
+                System.out.println("Programa Finalizado!");
+                break;
+            default:
+                System.out.println("Opção invalida");
+                break;
+        }
+        return opcao;
+    }
+}
